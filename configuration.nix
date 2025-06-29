2779c62758d57356396db67b2295891120190361
Emelie Graven <emelie@graven.se>
2025-06-29 14:49:47 +0200

    aaa

--- configuration.nix
+++ configuration.nix
@@ -4,11 +4,14 @@
     ./hardware-configuration.nix
   ];
 
+  users.defaultUserShell = pkgs.fish;
   users.users.emelie = {
     shell = pkgs.fish;
     isNormalUser = true;
     group = "emelie";
+    extraGroups = [ "wheel" ];
   };
+  users.groups.emelie = {}
   
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
